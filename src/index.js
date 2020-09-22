30438f2485a3c38d183f6ca16bae666eebdfa8ab
u0861355 <u0861355@utah.edu>
2020-09-22 11:05:21 -0600

    added null check to previous

--- src/index.js
+++ src/index.js
@@ -236,25 +236,23 @@ function create() {
 
 
         //ball touching ground
-        if ((bodyA.gameObject.texture.key == 'ball') && (bodyB.gameObject.texture.key == 'ground')) {
-            bodyA.gameObject.label = 'ballGrounded';
-            console.log("lable set to ballGrounded")
-
-        }
-        if ((bodyA.gameObject.texture.key == 'ground') && (bodyB.gameObject.texture.key == 'ball')) {
-            bodyB.gameObject.label = 'ballGrounded';
-            console.log("lable set to ballGrounded")
-
-        }
-        if ((bodyA.gameObject.texture.key == 'ball') && (bodyB.gameObject.texture.key == 'platform')) {
-            bodyA.gameObject.label = 'ballGrounded';
-            console.log("lable set to ballGrounded")
-
-        }
-        if ((bodyA.gameObject.texture.key == 'platform') && (bodyB.gameObject.texture.key == 'ball')) {
-            bodyB.gameObject.label = 'ballGrounded';
-            console.log("lable set to ballGrounded")
-
+        if (bodyA.gameObject != null && bodyB.gameObject != null) {
+            if ((bodyA.gameObject.texture.key == 'ball') && (bodyB.gameObject.texture.key == 'ground')) {
+                bodyA.gameObject.label = 'ballGrounded';
+                console.log("lable set to ballGrounded")
+            }
+            if ((bodyA.gameObject.texture.key == 'ground') && (bodyB.gameObject.texture.key == 'ball')) {
+                bodyB.gameObject.label = 'ballGrounded';
+                console.log("lable set to ballGrounded")
+            }
+            if ((bodyA.gameObject.texture.key == 'ball') && (bodyB.gameObject.texture.key == 'platform')) {
+                bodyA.gameObject.label = 'ballGrounded';
+                console.log("lable set to ballGrounded")
+            }
+            if ((bodyA.gameObject.texture.key == 'platform') && (bodyB.gameObject.texture.key == 'ball')) {
+                bodyB.gameObject.label = 'ballGrounded';
+                console.log("lable set to ballGrounded")
+            }
         }
         //end ball touching ground
 
@@ -342,24 +340,26 @@ function create() {
 
 
         //ball touching ground
-        if ((bodyA.gameObject.texture.key == 'ball') && (bodyB.gameObject.texture.key == 'ground')) {
-            bodyA.gameObject.label = 'ball';
-            console.log("lable set back to ball")
-        }
-        if ((bodyA.gameObject.texture.key == 'ground') && (bodyB.gameObject.texture.key == 'ball')) {
-            bodyB.gameObject.label = 'ball';
-            console.log("lable set back to ball")
+        if (bodyA.gameObject != null && bodyB.gameObject != null) {
+            if ((bodyA.gameObject.texture.key == 'ball') && (bodyB.gameObject.texture.key == 'ground')) {
+                bodyA.gameObject.label = 'ball';
+                console.log("lable set back to ball")
+            }
+            if ((bodyA.gameObject.texture.key == 'ground') && (bodyB.gameObject.texture.key == 'ball')) {
+                bodyB.gameObject.label = 'ball';
+                console.log("lable set back to ball")
 
-        }
-        if ((bodyA.gameObject.texture.key == 'ball') && (bodyB.gameObject.texture.key == 'platform')) {
-            bodyA.gameObject.label = 'ball';
-            console.log("lable set back to ball")
+            }
+            if ((bodyA.gameObject.texture.key == 'ball') && (bodyB.gameObject.texture.key == 'platform')) {
+                bodyA.gameObject.label = 'ball';
+                console.log("lable set back to ball")
 
-        }
-        if ((bodyA.gameObject.texture.key == 'platform') && (bodyB.gameObject.texture.key == 'ball')) {
-            bodyB.gameObject.label = 'ball';
-            console.log("lable set back to ball")
+            }
+            if ((bodyA.gameObject.texture.key == 'platform') && (bodyB.gameObject.texture.key == 'ball')) {
+                bodyB.gameObject.label = 'ball';
+                console.log("lable set back to ball")
 
+            }
         }
         //end ball touching ground
 
